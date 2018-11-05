5c2ec8e82dd456eaef2738ac43d03ee51d672c78
Wyatt Young <wyayoung@ucsc.edu>
2018-11-05 14:20:32 -0800

    whitman files

---
+++ bot2.js
@@ -0,0 +1,1 @@
+var twit = require('twit');
---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'JbASsP74kdfP1jXmpiITdsL0o',  
+  consumer_secret: 'JmLGY1TRhwT3fZvSgcgXeEiNUtqOsFbkyVgGxi0erG0ubmPBo4',
+  access_token: '806934077454491648-yPQAK5VTEycjlhUWui8wBzgZxxcoquj',  
+  access_token_secret: 'aXIMcXFiOMlqnPDzZAmU31NX6YvTachshNHKdNiCkpzNo'
+}
